7ebadcf019744aa5d15ab9680a583415a3f553cf
juanka <juanka588@hotmail.es>
2016-04-19 09:16:45 +0200

    bitwise

--- src/bit/BitwiseExercises.java
+++ src/bit/BitwiseExercises.java
@@ -2,5 +2,22 @@ package bit;
 
 public class BitwiseExercises {
 
+    /**
+     * You are given two 32-bit numbers, N and M, and two bit positions, i and
+     * j. Write a method to set all bits between i and j in N equal to M (e.g.,
+     * M becomes a substring of N located at i and starting at j).
+     */
+    public static int updateBits(int n, int m, int i, int j) {
+        int result;
+        System.out.println(Integer.toString(n, 2));
+        System.out.println(Integer.toString(m, 2));
+        int mask = ~(((1 << j) - 1) << i);
+        System.out.println(Integer.toString(mask, 2));
+        result = n & mask;
+        System.out.println(Integer.toString(result, 2));
+        result = result | (m << i);
+        System.out.println(Integer.toString(result, 2));
+        return result;
+    }
 
 }
--- test/bitwise/TestBitwiseExercises.java
+++ test/bitwise/TestBitwiseExercises.java
@@ -1,19 +1,35 @@
 package bitwise;
 
-import static org.junit.Assert.*;
+import bit.BitwiseExercises;
+import junit.framework.TestCase;
 
-import org.junit.Before;
-import org.junit.Test;
+public class TestBitwiseExercises extends TestCase {
 
-public class TestBitwiseExercises {
-
-    @Before
-    public void setUp() throws Exception {
+    protected void setUp() throws Exception {
+        super.setUp();
     }
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testUpdateBits() {
+        int n = Integer.parseInt("10000000000", 2), m = Integer.parseInt("10101", 2), i = 2, j = 6;
+        int result = BitwiseExercises.updateBits(n, m, i, j);
+        assertEquals(Integer.parseInt("10001010100", 2), updateBits(n, m, i, j));
+        assertEquals(Integer.parseInt("10001010100", 2), result);
+    }
+
+    public int updateBits(int n, int m, int i, int j) {
+        int max = ~0; /* All 1’s */
+
+        // 1’s through position j, then 0’s
+        int left = max - ((1 << j) - 1);
+
+        // 1’s after position i
+        int right = ((1 << i) - 1);
+
+        // 1’s, with 0s between i and j
+        int mask = left | right;
+
+        // Clear i through j, then put m in there
+        return (n & mask) | (m << i);
     }
 
 }
